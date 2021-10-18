8b0383d1b3fc512ad1b54a2a337273198df158fd
NefedovAndry <nefedov_andry@bk.ru>
2021-10-18 23:18:41 +0300

    hw_7 refactoring Main, add ShapeName

--- src/main/java/module_7/Main.java
+++ src/main/java/module_7/Main.java
@@ -2,9 +2,10 @@ package module_7;
 
 public class Main {
     public static void main(String[] args) {
-        Shape[] shapeArray = new Shape[]{new Rectangle(), new Quad(), new Rhomb(), new Ellipse(), new Circle(), new Line(), new Point()};
+        Shape[] shapeArray = new Shape[]{new Rectangle(), new Quad(), new Rhomb(), new Ellipse(), new Circle(),
+                new Line(), new Point()};
         for (Shape shape : shapeArray) {
-            System.out.println(shape.getName());
+            new ShapeName(shape);
         }
     }
 }
---
+++ src/main/java/module_7/ShapeName.java
@@ -0,0 +1,7 @@
+package module_7;
+
+public class ShapeName {
+    public ShapeName(Shape shape) {
+        System.out.println(shape.getName());
+    }
+}
